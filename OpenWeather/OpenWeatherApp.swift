018fc11506ec067d4510876c2f10e06aba801c2b
ewrika <egorkaomsk_2003@mail.ru>
2024-01-17 14:48:26 +0600

    Initial Commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Георгий Борисов on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
